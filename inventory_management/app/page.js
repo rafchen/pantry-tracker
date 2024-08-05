ad330bdc3a8777b0db547c4288e0cd8b39bd674e
Rafael Chen <rafaelchen@Rafaels-MBP-2.lan>
2024-08-04 23:55:42 -0400

    downloaded packages

--- inventory_management/app/page.js
+++ inventory_management/app/page.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { Box, Stack, Typography, Button, Modal, TextField, Grid } from '@mui/material';
 import { firestore } from '../app/firebase';
 import { collection, doc, getDocs, query, setDoc, deleteDoc, getDoc } from 'firebase/firestore';
-import { debounce } from 'lodash';
+import debounce from 'lodash/debounce';
 const {
   GoogleGenerativeAI,
   HarmCategory,
